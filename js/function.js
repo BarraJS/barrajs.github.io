5ef2d4279a2e2c6ebf4cfd5e3fe3f6d30a369863
Danilo Reis <daniloreisbarreto@globomail.com>
2015-04-01 11:06:46 -0300

    Adiciono 1 º versão do site.

---
+++ js/function.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function($) { 
+	$(".scroll").click(function(event){		
+		event.preventDefault();
+		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
+	});
+});

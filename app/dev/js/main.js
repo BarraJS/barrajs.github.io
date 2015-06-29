6933e81d276a224bc6af4a31229ecb72c33b64a0
Bruno Rodrigues <brunodesign1206@gmail.com>
2015-06-29 03:36:29 -0300

    Add slide slick para transicao dos palestrantes

--- app/dev/js/main.js
+++ app/dev/js/main.js
@@ -1,1 +1,34 @@
 // Main Scripts
+var BarraJS = {} || window.BarraJS;
+
+BarraJS.slideTalkers = function() {
+	var talkers = $('#talkers .list-talkers');
+
+	// Add slide(slick) aos palestrantes
+	talkers.slick({
+		arrows: true,
+		dots: true,
+		slidesToShow: 2,
+		slidesToScroll: 2,
+		autoplay: true,
+		autoplaySpeed: 5000,
+
+		// Responsive
+		responsive: [{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				infinite: true,
+				dots: true,
+				arrows: false
+			}
+		}]
+	});
+};
+
+// Load Scripts
+$(document).ready(function(){
+	// Slide com Palestrantes
+	BarraJS.slideTalkers();
+});

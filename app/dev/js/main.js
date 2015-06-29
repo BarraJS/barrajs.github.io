62361c72b98318b5a2fa5fe3dc84597a1d3d0def
Bruno Rodrigues <brunodesign1206@gmail.com>
2015-06-29 04:03:04 -0300

    Crio modulo para scroll entre ancoras na pagina

--- app/dev/js/main.js
+++ app/dev/js/main.js
@@ -1,10 +1,11 @@
 // Main Scripts
 var BarraJS = {} || window.BarraJS;
 
+// Slide para palestrantes
 BarraJS.slideTalkers = function() {
 	var talkers = $('#talkers .list-talkers');
 
-	// Add slide(slick) aos palestrantes
+	// Slick para criar slide
 	talkers.slick({
 		arrows: true,
 		dots: true,
@@ -27,8 +28,33 @@ BarraJS.slideTalkers = function() {
 	});
 };
 
+// Modulo para ancoras na pagina
+BarraJS.anchorPoint = {
+
+	// Principal - Scroll da pagina
+	main : function(el) {
+		$('html, body').animate({
+			scrollTop: $($(el).data('target')).offset().top
+		}, 800);
+		return false;
+	},
+
+	// Atribuindo eventos
+	bindEvents : function() {
+		$('.anchor').on('click', function(){
+			BarraJS.anchorPoint.main(this);
+		});
+	},
+
+	// Init dos metodos
+	init : function() {
+		BarraJS.anchorPoint.bindEvents();
+	}
+};
+
+
 // Load Scripts
 $(document).ready(function(){
-	// Slide com Palestrantes
-	BarraJS.slideTalkers();
+	BarraJS.slideTalkers(); // Slide com Palestrantes
+	BarraJS.anchorPoint.init(); // Modulo para ancoras
 });
